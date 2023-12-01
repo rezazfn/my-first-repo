3de4dc631bfb943c56cfb7d42bab3c77c4bc919b
rezazfn <s10256015@connect.np.edu.sg>
2023-12-01 17:27:27 +0800

    Checkpoint

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementsByClassName('send:after')[0].addEventListener("click", myPopupFunction);
+
+function myPopupFunction() {
+  alert('Notified! We will send you a notication for any updates.')
+}
